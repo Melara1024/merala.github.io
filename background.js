f7e3b12fa16552378a5bf22af52de969b532ad9e
Melara <nitrogen1456@gmail.com>
2022-09-19 20:52:46 +0900

    fix

--- background.js
+++ background.js
@@ -2,19 +2,19 @@ function changeBackground( name ) {
     switch(name)
     {
         case "bricks":
-            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://melara1024.github.io/site/resources/background/sand.png)";
+            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://github.com/Melara1024/site/blob/main/resources/background/sand.png)";
         break;
         case "sand":
-            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://melara1024.github.io/site/resources/background/sand.png)";
+            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://github.com/Melara1024/site/blob/main/resources/background/sand.png)";
         break;
         case "iron":
-            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://melara1024.github.io/site/resources/background/iron_block.png)";
+            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://github.com/Melara1024/site/blob/main/resources/background/iron_block.png)";
         break;
         case "deep":
-            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://melara1024.github.io/site/resources/background/deepslate_top.png)";
+            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://github.com/Melara1024/site/blob/main/resources/background/deepslate_top.png)";
         break;
         case "barrel":
-            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://melara1024.github.io/site/resources/background/barrel_bottom.png)";
+            document.getElementsByTagName('body')[0].style.backgroundImage = "url(https://github.com/Melara1024/site/blob/main/resources/background/barrel_bottom.png)";
         break;
     }
 }
