0d8007717228d2a8499c0f9904b8087d59ce1b3d
Melara <nitrogen1456@gmail.com>
2022-09-20 13:21:12 +0900

    change csstab to inputtab

--- menu.js
+++ menu.js
@@ -1,15 +1,25 @@
 (function($) {
     $(function() {
-        var $header = $('#head_wrap');
+        var $article = $('.article');
+        var $header = $('.header');
         // Nav Fixed
         $(window).scroll(function() {
             if ($(window).scrollTop() > 350) {
-                $header.addClass('fixed');
+                $article.addClass('fixed');
+                $article.addClass('fixed');
             } else {
+                $header.removeClass('fixed');
                 $header.removeClass('fixed');
             }
         });
         // Nav Toggle Button
+        $('#nav-toggle').click(function(){
+            $article.toggleClass('open');
+        });
+
+        $('#nav-closer').click(function(){
+            $article.removeClass('open');
+        });
         $('#nav-toggle').click(function(){
             $header.toggleClass('open');
         });
